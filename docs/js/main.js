a542668aecc133d09557710511bc7a270d01507e
Andry <andry.yosua@pinteraktif.com>
2017-03-03 18:10:49 +0700

    update docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,6 +2,10 @@
 
 var app = new Vue({
     el: '#app',
+    created: function created() {
+        this.todos = JSON.parse(!!localStorage['todos'] ? localStorage['todos'] : '[]');
+    },
+
     data: {
         todos: [
             // {id: 2, text: 'sample 1', isDone: true},
@@ -18,9 +22,14 @@ var app = new Vue({
         }
     },
     methods: {
+        setFilterTodo: function setFilterTodo(value) {
+            this.filterTxt = value;
+            window.location.hash = '#' + value;
+        },
         filterTodo: function filterTodo(value, status) {
             // console.log(value);
             var a = value;
+            if (!!window.location.hash) this.filterTxt = window.location.hash.slice(1);
             switch (!status ? this.filterTxt : status) {
                 case 'completed':
                     a = value.filter(function (el) {
@@ -42,6 +51,7 @@ var app = new Vue({
             var maxId = Math.max.apply(Math, !!ids.length ? ids : [0]) + 1;
             this.todos.push({ id: maxId, text: e.target.value, isDone: false });
             e.target.value = '';
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         onEditTodo: function onEditTodo(e) {
             e.target.innerHTML = '<input type="text" value="' + e.target.innerText + '" @keyup.enter="onSubmitEditTodo"/>';
@@ -54,6 +64,7 @@ var app = new Vue({
                 return el.id === parseInt(e.target.closest('.todo').querySelector('input[type="checkbox"]').value);
             });
             this.todos.splice(i, 1);
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         onCheckTodo: function onCheckTodo(e) {
             var i = this.todos.findIndex(function (el) {
@@ -61,6 +72,7 @@ var app = new Vue({
             });
             this.todos[i].isDone = e.target.checked;
             e.target.parentNode.querySelector('i.material-icons').innerHTML = e.target.checked ? 'check_box' : 'check_box_outline_blank';
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         onDeleteCompletedTode: function onDeleteCompletedTode(e) {
             this.todos = this.todos.filter(function (el) {
