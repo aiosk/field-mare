714321504b91374b8dc906dc0e5ec45a99b37b78
Andry <andry.yosua@pinteraktif.com>
2017-03-10 17:18:07 +0700

    update docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,27 +1,94 @@
 'use strict';
 
+var saveToLocalStorage = function saveToLocalStorage(value) {
+    localStorage.setItem('todos', JSON.stringify(value));
+};
+var todoInput = {
+    props: ['todos'],
+    methods: {
+        addTodo: function addTodo(e) {
+            var ids = this.todos.map(function (el) {
+                return el.id;
+            });
+            var maxId = Math.max.apply(Math, !!ids.length ? ids : [0]) + 1;
+
+            this.todos.push({
+                id: maxId,
+                text: window.btoa(e.target.value),
+                isDone: false
+            });
+            e.target.value = '';
+
+            saveToLocalStorage(this.todos);
+        }
+    }
+};
+
+var todoList = {
+    props: ['todo', 'i', 'todos'],
+    methods: {
+        onEditTodo: function onEditTodo(e) {
+            var $el = e.target;
+            $el.parentNode.querySelector('input').classList.remove('hide');
+            $el.classList.add('hide');
+            $el.parentNode.querySelector('input').focus();
+        },
+        onSubmitEditTodo: function onSubmitEditTodo(e) {
+            var $el = e.target;
+            $el.parentNode.querySelector('div').classList.remove('hide');
+            $el.classList.add('hide');
+
+            saveToLocalStorage(this.todos);
+        },
+
+        onDeleteTodo: function onDeleteTodo(e) {
+            var i = this.todos.findIndex(function (el) {
+                return el.id === parseInt(e.target.closest('.todo').querySelector('input[type="checkbox"]').value);
+            });
+            this.todos.splice(i + 1, 1);
+
+            saveToLocalStorage(this.todos);
+        },
+        onCheckTodo: function onCheckTodo(e) {
+            var $el = e.target;
+            var i = this.todos.findIndex(function (el) {
+                return el.id === parseInt(e.target.value);
+            });
+            this.todos[i].isDone = e.target.checked;
+            $el.parentNode.querySelector('i.material-icons').innerHTML = e.target.checked ? 'check_box' : 'check_box_outline_blank';
+
+            saveToLocalStorage(this.todos);
+        }
+    }
+};
+
 var app = new Vue({
     el: '#app',
+    components: {
+        'todo-input': todoInput,
+        'todo-list': todoList
+    },
     data: {
-        todos: [
-            // {id: 2, text: 'sample 1', isDone: true},
-            // {id: 1, text: 'sample 2', isDone: false}
-        ],
+        todos: [],
         filterTxt: 'all'
     },
+    created: function created() {
+        this.todos = JSON.parse(!!localStorage['todos'] ? localStorage['todos'] : '[]');
+    },
+
     computed: {
         todoLeft: function todoLeft() {
             var itemLeft = this.filterTodo(this.todos).filter(function (o) {
                 return !o.isDone;
-            }).length;
+            }).length + 1;
             return itemLeft + ' item' + (itemLeft > 1 ? 's' : '') + ' left';
         }
     },
     methods: {
         filterTodo: function filterTodo(value, status) {
-            // console.log(value);
             var a = value;
-            switch (!status ? this.filterTxt : status) {
+            if (!!window.location.hash) this.filterTxt = window.location.hash.slice(1);
+            switch (status ? this.filterTxt : status) {
                 case 'completed':
                     a = value.filter(function (el) {
                         return el.isDone;
@@ -35,37 +102,20 @@ var app = new Vue({
             }
             return a;
         },
-        onAddTodo: function onAddTodo(e) {
-            var ids = this.todos.map(function (el) {
-                return el.id;
-            });
-            var maxId = Math.max.apply(Math, !!ids.length ? ids : [0]) + 1;
-            this.todos.push({ id: maxId, text: e.target.value, isDone: false });
-            e.target.value = '';
-        },
-        onEditTodo: function onEditTodo(e) {
-            e.target.innerHTML = '<input type="text" value="' + e.target.innerText + '" @keyup.enter="onSubmitEditTodo"/>';
-            e.target.querySelector('input').focus();
-        },
-        onSubmitEditTodo: function onSubmitEditTodo(e) {},
-
-        onDeleteTodo: function onDeleteTodo(e) {
-            var i = this.todos.findIndex(function (el) {
-                return el.id === parseInt(e.target.closest('.todo').querySelector('input[type="checkbox"]').value);
-            });
-            this.todos.splice(i, 1);
-        },
-        onCheckTodo: function onCheckTodo(e) {
-            var i = this.todos.findIndex(function (el) {
-                return el.id === parseInt(e.target.value);
-            });
-            this.todos[i].isDone = e.target.checked;
-            e.target.parentNode.querySelector('i.material-icons').innerHTML = e.target.checked ? 'check_box' : 'check_box_outline_blank';
-        },
-        onDeleteCompletedTode: function onDeleteCompletedTode(e) {
+        onDeleteCompletedTodo: function onDeleteCompletedTodo(e) {
             this.todos = this.todos.filter(function (el) {
                 return !el.isDone;
             });
+
+            saveToLocalStorage(this.todos);
+        },
+        isActive: function isActive(value) {
+            return !!window.location.hash && window.location.hash.slice(1) === value;
+        },
+        setFilterTodo: function setFilterTodo(e) {
+            var value = e.target.innerText.trim();
+            this.filterTxt = value;
+            window.location.hash = '#' + value;
         }
     }
 });
